18ef924f48348f0d1452632349933350f1a84e55
studentmarvel1 <89390722+studentmarvel1@users.noreply.github.com>
2021-09-14 14:29:25 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', (e) => {
+  e.preventDefault();
+document.querySelector('#my-form').getElementsByClassName.background = '#ccc';
+});
